7507ba989b4a6753309693d2dd74c0c054853f5d
John314-g <jmalambo1542@gmail.com>
2024-11-26 15:42:45 -0500

    se añadio firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.application) // Esta es suficiente
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services") // Este es necesario para Firebase
 }
 
 android {
@@ -10,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.ud.serpientesescalerasmvvm"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -45,4 +46,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0")) // Mantén esto para Firebase
 }
